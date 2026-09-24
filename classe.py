ac00067eb6f3c8b22bf567be435768d49b6755a3
nettofabriciox <agostinhonetto73@gmail.com>
2026-09-24 16:35:13 -0300

    Orientação Objetos - Classe-inicialização de varaiaveis

--- classe.py
+++ classe.py
@@ -1,5 +1,12 @@
-class MinhaClass:
-    x = 20
+#class MinhaClass:
+ #   x = 20
+#p1 = MinhaClass()
+#print(p1.x)  
+# 
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-p1 = MinhaClass()
-print(p1.x)    
+p1 = Person('Jose', 45)
+print(p1.name, 'Tem anos: ', p1.age)
